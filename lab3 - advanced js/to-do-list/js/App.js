0d56df4768c2fe8ebcbb6abefbd336729328a587
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-04 09:04:38 +0100

    refactor: removed the hint comments the teacher gave

--- lab3 - advanced js/to-do-list/js/App.js
+++ lab3 - advanced js/to-do-list/js/App.js
@@ -3,38 +3,18 @@ import Todo from "./Todo";
 export default class App {
   constructor() {
     console.log("🍕");
-    // HINT🤩
-    // set up the enter Key
-    // this.setupEventListeners();
-    // when the app loads, we can show previously saved items from localstorage
-    // this.loadFromStorage();
     this.setupEventListeners();
     this.loadFromStorage();
   }
 
   setupEventListeners() {
     console.log("👂🏽");
-    // HINT🤩
-    // pressing the enter key in the text field triggers the createItem function
-    // addEventListener("keyup", this.createItem.bind(this));
-    // read up on .bind() -> we need to pass the current meaning of this to the eventListener
-    // while testing, feel free to console.log(this) to see what's in it
-
     document
       .querySelector("#add-item-text")
       .addEventListener("keyup", this.createItem.bind(this));
   }
 
   createItem(e) {
-    console.log("📕");
-    // HINT🤩
-    // this function should create a new todo by using the Todo() class
-    // new Todo(text)
-    // todo.add();
-    // todo.saveToStorage();
-    // if you used bind() in the previous function, you'll notice that this refers to the current class instance
-    // clear the text field with .reset() after adding the item
-    // if (e.key === "Enter")
     let text = document.getElementById("add-item-text").value.trim();
 
     if (e.key === "Enter") {
@@ -50,9 +30,6 @@ export default class App {
   }
 
   loadFromStorage() {
-    // HINT🤩
-    // load all items from storage here and add them to the screen
-    // use the Todo class to create the elements
     if (localStorage.getItem("list") != null) {
       let list = JSON.parse(localStorage.getItem("list"));
       for (let i = 0; i < list.length; i++) {
@@ -63,7 +40,6 @@ export default class App {
   }
 
   reset() {
-    // this function should reset the form / clear the text field
     document.getElementById("add-item-text").value = "";
   }
 }
--- lab3 - advanced js/to-do-list/js/Todo.js
+++ lab3 - advanced js/to-do-list/js/Todo.js
@@ -2,21 +2,10 @@ let listArray = JSON.parse(localStorage.getItem("list")) || [];
 
 export default class Todo {
   constructor(title) {
-    // HINT🤩
-    // use a constructor to set basic property values
     this.title = title;
   }
 
   createElement() {
-    // HINT🤩
-    // this method will create the HTML structure with the correct classes, based on the todo priority
-    // let newNote = document.createElement("li");
-    // check if the todo item includes a priority like medium: to generate the correct classnames
-    // don't forget to hook up an event listener for the click event
-    // return newNote;
-
-    //   localStorage.removeItem('list');
-
     let li = document.createElement("li");
 
     const front = this.title.substring(0, this.title.indexOf(":"));
@@ -46,59 +35,28 @@ export default class Todo {
   }
 
   markDone() {
-    // HINT🤩
-    // this function should mark the current todo as done, by adding the correct CSS class
-    // if the item is clicked, but was already marked as done, remove the item from the list
-  
     if (this.classList.contains("done")) {
       document.getElementById("todo-list").removeChild(this);
       let todos;
-      if(localStorage.getItem('list') === null){
-      todos = [];
-      }
-      else {
-      todos = JSON.parse(localStorage.getItem('list'));
+      if (localStorage.getItem("list") === null) {
+        todos = [];
+      } else {
+        todos = JSON.parse(localStorage.getItem("list"));
       }
       const index = this.innerText;
       todos.splice(todos.indexOf(index), 1);
-      localStorage.setItem('list', JSON.stringify(todos));
-      // localStorage.removeItem(this.title);
-      // localStorage.removeItem();
-      // let list = JSON.parse(localStorage.getItem('list'));
-      // let checked = list.indexOf(this.outerHTML.replace(/(<([^>]+)>)/gi, ""));
-      // let newList = list.splice(checked, checked);
-      // if(!rewrite){
-
-      //}
-      // else{
-      //     localStorage.removeItem('list');
-      // }
-      // localStorage.setItem('list', '[]');
-      // list.splice(list.findIndex(e => e.name === this.outerHTML.replace(/(<([^>]+)>)/gi, "")),1);
-      // console.log(list);
-      // localStorage.setItem('list', JSON.stringify(listArray));
-
-      // console.log(list);
-      // localStorage.setItem('list', list);
-      // list.pop();
+      localStorage.setItem("list", JSON.stringify(todos));
     } else {
       this.classList.add("done");
     }
-   }
+  }
 
   add() {
-    // HINT🤩
-    // this function should append the note to the screen somehow
-    let todo = this.createElement(); // should return a full <li> with the right classes and innerHTML
-
+    let todo = this.createElement();
     document.getElementById("todo-list").appendChild(todo);
   }
 
   saveToStorage() {
-    // HINT🤩
-    // localStorage only supports strings, not arrays
-    // if you want to store arrays, look at JSON.parse and JSON.stringify
-
     localStorage.setItem("list", "[]");
     listArray.push(this.title);
     localStorage.setItem("list", JSON.stringify(listArray));
