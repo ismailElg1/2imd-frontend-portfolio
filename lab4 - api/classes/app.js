c2da404ee95c45d48b3877513f2dac6f4805eeb7
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-09 19:08:30 +0100

    feat: made connection with weather visual crossing api with according location

--- lab4 - api/classes/app.js
+++ lab4 - api/classes/app.js
@@ -1,14 +1,32 @@
 export default class App{
     constructor(){
         this.getLocation();
+        this.lat;
+        this.lng;
     }
     getLocation(){
-        navigator.geolocation.getCurrentPosition(this.locationDone, this.locationFailed);
+        navigator.geolocation.getCurrentPosition(this.locationDone.bind(this),  this.locationFailed.bind(this));
     }
     locationDone(result){
-        console.log(result);
+        this.lat = result.coords.latitude;
+        this.lng = result.coords.longitude;
+        this.getWeather();
+        
     }
     locationFailed(err){
         console.log(err);
+    }
+    getWeather(){
+        let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.lat}%2C%20${this.lng}?unitGroup=metric&key=7CHVDNAGSRHWQ4HVSFUJYFYUY&contentType=json`;
+        fetch(url).then(response => {
+            console.log(response);
+            response.json();
+         
+        }).then(data => {
+            console.log(data);
+        }).catch(err=>{
+            console.log(err);
+        });
+        console.log();
     }
 }
