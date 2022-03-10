275b1b4d855c6b2ea50de6232e2dcaec28d1cddb
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-10 11:30:21 +0100

    refactor: changed the way it checks url

--- lab5 - node/index.js
+++ lab5 - node/index.js
@@ -4,26 +4,48 @@
 //     text: "Nodejs, lets doooo this!",
 // }));
 
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-    switch(req.url){
-        case "/signup":
-            case "/singup": 
-                res.end('This is the signup page!');
-            break;
+// const server = http.createServer((req, res) => {
+//     switch(req.url){
+//         case "/signup":
+//             case "/singup": 
+//                 res.end('This is the signup page!');
+//             break;
 
-            default: 
-                res.end("Hello World");
-    }
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+//             default: 
+//                 res.end("Hello World");
+//     }
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+
+  app.get('api/v1/todos', (req, res) => {
+    res.send("get todos");
+  });
+
+  // GET api/v1/todos
+  // POST api/v1/todos
+  // PUT api/v1/todos/3
+  // DEL api/v1/todos/3
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
