996a6f8656085d857fd8583fc6d0565318a9dc3d
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-16 19:38:34 +0100

    feat: added middleware folder and middleware function

--- lab5 - node/index.js
+++ lab5 - node/index.js
@@ -28,6 +28,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const logger = require('./middleware/logger')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
