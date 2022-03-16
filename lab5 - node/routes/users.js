7d3f307b848acc28662ece33ec7cb62adbed1a5e
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-16 23:21:13 +0100

    feat: added passport.js lib and prepared authentication files

--- lab5 - node/routes/users.js
+++ lab5 - node/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+const authController = require("../controllers/auth");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/signup', (req, res, next) => {
+  //signup user
+
+});
 module.exports = router;
