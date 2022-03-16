a46aa47bd97b3706d53f5cb01f59742bfb021f8a
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-16 19:21:21 +0100

    feat: made the connection work using postman

--- lab5 - node/index.js
+++ lab5 - node/index.js
@@ -4,45 +4,57 @@
 //     text: "Nodejs, lets doooo this!",
 // }));
 
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-    switch(req.url){
-            case "/signup":
-                res.end('This is the signup page!');
-            break;
-            default: 
-                res.end("Hello World");
-    }
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//     switch(req.url){
+//             case "/signup":
+//                 res.end('This is the signup page!');
+//             break;
+//             default: 
+//                 res.end("Hello World");
+//     }
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api/v1/todos', (req, res) => {
+  res.send("GET todos");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.post('/api/v1/todos', (req, res) => {
+  res.send("POST todos");
 });
 
-// const express = require('express')
-// const app = express()
-// const port = 3000
+app.put('/api/v1/todos', (req, res) => {
+  res.send("PUT todos");
+});
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
+app.delete('/api/v1/todos', (req, res) => {
+  res.send("DELETE todos");
+});
 
-//   app.get('/api/v1/todos', (req, res) => {
-//     res.send("get todos");
-//   });
-
+ 
   // GET api/v1/todos
   // POST api/v1/todos
   // PUT api/v1/todos/3
   // DEL api/v1/todos/3
 
-// })
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
