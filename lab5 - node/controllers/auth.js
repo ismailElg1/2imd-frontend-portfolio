4171fa5b573504666d15da5e10531890af402fae
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-17 10:31:42 +0100

    feat: you are now able to use the api and login through frontend

--- lab5 - node/controllers/auth.js
+++ lab5 - node/controllers/auth.js
@@ -28,10 +28,22 @@ const signup = async (req, res, next) => {
 
 const login = async (req, res, next) => {
     const user = await User.authenticate()(req.body.username, req.body.password).then(result => {
-        res.json({
+
+        
+        if(!result.user){
+            return res.json({
+                "status": "failed",
+                "message": "Login failed"
+            })
+        }
+        let token = jwt.sign({
+            uid: result.user._id,
+            username: result.user.username
+        }, "MyVerySecretWord");
+        return res.json({
             "status": "success", 
             "data":{
-                "user" : result
+                "token" : token
             }
         })
     }).catch(error =>{
