b645fb83fc51aeaa36e50885293ee66418e50847
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-17 11:54:17 +0100

    feat: you can now mark a to do list as done

--- lab5 - node/controllers/api/v1/todos.js
+++ lab5 - node/controllers/api/v1/todos.js
@@ -17,7 +17,6 @@ const create =  (req, res, next) => {
     
     let todo = new Todo();
     todo.text = req.body.text;
-    console.log(req.user);
     todo.user = req.user._id;
     todo.completed = false;
     todo.save((err, doc) => {
@@ -39,6 +38,28 @@ const create =  (req, res, next) => {
 
 }
 
+const update =  (req, res) => {
+    let user = req.user._id;
+    let todoId = req.params.id;
+    Todo.findOneAndUpdate({
+        user: user,
+        _id: todoId
+    }, {
+        completed: true
+    }, {new: true}
+    ).then(doc =>{
+       res.json({
+        "status": "success",
+        "data": {
+            todo: doc
+        } 
+       })
+    }).catch(err => {
+        res.json(err);
+    })
+}
+
+
 module.exports.getAll = getAll;
 module.exports.create = create;
-
+module.exports.update = update;
--- lab5 - node/routes/api/v1/todos.js
+++ lab5 - node/routes/api/v1/todos.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const todosController = require('../../../controllers/api/v1/todos');
 
 router.get("/", todosController.getAll);
-
 router.post("/", todosController.create);
+router.put('/:id', todosController.update);
 
 module.exports = router;
